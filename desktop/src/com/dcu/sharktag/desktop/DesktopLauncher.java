3ef678f2177ede187da9752d9765e3b3bcb7ef23
Jerzy Baran <1chmsid@gmail.com>
2016-02-03 12:44:46 +0000

    Fixed font rendering on desktop.

--- desktop/src/com/dcu/sharktag/desktop/DesktopLauncher.java
+++ desktop/src/com/dcu/sharktag/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.dcu.sharktag.SharkTag;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 800;
+		config.height = 600;
 		new LwjglApplication(new SharkTag(), config);
 	}
 }
