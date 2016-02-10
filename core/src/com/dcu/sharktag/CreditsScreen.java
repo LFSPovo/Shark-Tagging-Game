59bffa1f9ff053dc474c840713c0f5516999cf81
Jerzy Baran <1chmsid@gmail.com>
2016-02-10 12:49:15 +0000

    Added credits

--- core/src/com/dcu/sharktag/CreditsScreen.java
+++ core/src/com/dcu/sharktag/CreditsScreen.java
@@ -3,6 +3,8 @@ package com.dcu.sharktag;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
@@ -20,11 +22,15 @@ public class CreditsScreen extends ScreenAdapter{
 	private float uiOriginX = 0;
 	private float uiOriginY = 0;
 	
+	private SpriteBatch batch;
+	private Texture credits;
+	
 	public CreditsScreen(SharkTag game){
 		this.game = game;
 		
 		uiOriginX = game.WORLD_WIDTH / 2;
 		uiOriginY = 4.5f * game.WORLD_HEIGHT / 5;
+//		Gdx.gl.
 	}
 	
 	@Override
@@ -32,12 +38,19 @@ public class CreditsScreen extends ScreenAdapter{
 		stage = new Stage(new FitViewport(game.WORLD_WIDTH, game.WORLD_HEIGHT));
 		Gdx.input.setInputProcessor(stage);
 		
+		batch = new SpriteBatch();
+		credits = new Texture(Gdx.files.internal("ui/credits2.png"));
+		
+//		Gdx.gl.glClearColor(1, 0, 0, 1);
+		
 		buildGUI();
 	}
 	
 	@Override
 	public void render(float delta){
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		draw();
 		stage.act();
 		stage.draw();
 	}
@@ -53,13 +66,14 @@ public class CreditsScreen extends ScreenAdapter{
 	
 	@Override
 	public void dispose(){
+		credits.dispose();
 		stage.dispose();
 	}
 	
 	private void buildGUI(){
-		Label programmingHeading = new Label("Programming", game.getUISkin());
-		programmingHeading.setPosition(uiOriginX, uiOriginY, Align.center);
-		stage.addActor(programmingHeading);
+//		Label programmingHeading = new Label("Programming", game.getUISkin());
+//		programmingHeading.setPosition(uiOriginX, uiOriginY, Align.center);
+//		stage.addActor(programmingHeading);
 		
 		TextButton backButton = new TextButton("Back", game.getUISkin());
 		backButton.setPosition(uiOriginX, 50, Align.center);
@@ -73,5 +87,14 @@ public class CreditsScreen extends ScreenAdapter{
 				dispose();
 			}
 		});
+	}
+	
+	private void draw(){
+		batch.setProjectionMatrix(stage.getCamera().projection);
+		batch.setTransformMatrix(stage.getCamera().view);
+		
+		batch.begin();
+		batch.draw(credits, 0, 0);
+		batch.end();
 	}
 }
