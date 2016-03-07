1f231cb604fb5bda45d90cb3595924850b1e56d4
Jerzy Baran <1chmsid@gmail.com>
2016-03-07 11:27:08 +0000

    does not upload the empty tag

--- core/src/com/dcu/sharktag/ServerRequests/TagRequest.java
+++ core/src/com/dcu/sharktag/ServerRequests/TagRequest.java
@@ -12,10 +12,15 @@ public class TagRequest {
 	public TagRequest(String token, String imageId, Array<Tag> tags) {
 		this.token = token;
 		this.imageId = imageId;
-		this.tags = new SimpleTag[tags.size];
+		Array<SimpleTag> tmpArray = new Array<SimpleTag>();
 		
 		for(int i = 0; i < tags.size; i++){
-			this.tags[i] = tags.get(i).toSimpleTag();
+			if(tags.get(i).getSharkId() == 0){
+				continue;
+			}
+			tmpArray.add(tags.get(i).toSimpleTag());
 		}
+		
+		this.tags = tmpArray.toArray(SimpleTag.class);
 	}
 }
